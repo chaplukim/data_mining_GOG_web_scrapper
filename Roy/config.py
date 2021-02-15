ba45b3976c6dc3ea7d99631ee20ab6f15f75e33b
Roy Toledano <roy.toled@gmail.com>
2021-02-15 11:54:47 +0200

    first_try_15022021

---
+++ Roy/config.py
@@ -0,0 +1,8 @@
+"""
+GOG Config file
+"""
+gog_url = "https://www.gog.com/games?page=1&sort=popularity"
+
+divinity_game_example = "https://www.gog.com/game/divinity_original_sin_2"
+relevant_keys = ['genre', 'works_on', 'release_date', 'company', 'size']
+
