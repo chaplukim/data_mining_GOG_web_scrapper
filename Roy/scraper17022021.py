d16e48b3f1325d194f1f96fc34db5cca2bafdf9e
Roy Toledano <roy.toled@gmail.com>
2021-02-17 19:14:37 +0200

    17022021

--- Roy/scraper17022021.py
+++ Roy/scraper17022021.py
@@ -13,72 +13,98 @@ import os
 
 
 def clean_key(key):
-    """Cleans the key of the dict. That key need to be the field name in the sql"""
-    key_cleaner = str(key)
-    key_cleaner = key_cleaner.lower()
-    key_cleaner = re.sub(r'[:]', '', key_cleaner.strip())
-    if re.match(r'\w+ \w+', key_cleaner):
-        key_cleaner = re.sub(' ','_',key_cleaner)
+    """Cleans the field name.
+     That key need to be the field name in the sql"""
+    try:
+        key_cleaner = str(key)
+        key_cleaner = key_cleaner.lower()
+        key_cleaner = re.sub(r'[:]', '', key_cleaner.strip())
+        if re.match(r'\w+ \w+', key_cleaner):
+            # adding underscore between two words (for SQL columns' names)
+            key_cleaner = re.sub(' ', '_', key_cleaner)
+    except:
+        return "NULL"
     return key_cleaner
 
 
 #  Game Details
-def game_gnere_cleaner(row):
-    """returns the game gneres lowercase"""
-    game_gnere = row.text.strip()
-    game_gnere = re.sub(r'[\n]', '', game_gnere)
-    game_gnere = game_gnere.split(' - ')
-    for idx, game in enumerate(game_gnere):
-        game_gnere[idx] = game.strip()
-    return game_gnere
+def game_genre_cleaner(row):
+    """Returns the game genre"""
+    new_line_regex = r'[\n]'
+    try:
+        game_genre = row.text.strip()
+        game_genre = re.sub(new_line_regex, '', game_genre)
+        game_genre = game_genre.split(' - ')
+        for idx, game in enumerate(game_genre):
+            game_genre[idx] = game.strip()
+    except:
+        return "NULL"
+    return game_genre
+
 
 def works_on_cleaner(row):
-    """Operating system cleaner"""
-    works_on = row.text.lower().strip()
+    """Returns on which OSs' the game is working"""
+    try:
+        works_on = row.text.lower().strip()
+    except:
+        return 'NULL'
     return works_on
 
 
 def release_date_cleaner(row):
-    """Returns game release date"""
-    release_date = re.search(r'\d{4}-\d{2}-\d{2}', row.text).group()
-    release_date = datetime.datetime.strptime(release_date, '%Y-%m-%d')
+    """Returns the game's release date YYYY-mm-dd (datetime)"""
+    datetime_regex = r'\d{4}-\d{2}-\d{2}'
+    try:
+        release_date = re.search(datetime_regex, row.text).group()
+        release_date = datetime.datetime.strptime(release_date, '%Y-%m-%d')
+    except:
+        return "NULL"
     return release_date
 
 
 def company_cleaner(row):
-    company = row.text.lower()
-    company = re.sub(r'[\n]', '', company)
-    company = company.split(r'/')
-    for idx, comp in enumerate(company):
-        company[idx] = comp.strip()
+    """Returns the game's company & publisher names (list)"""
+    try:
+        company = row.text.lower()
+        company = re.sub(r'[\n]', '', company)
+        company = company.split(r'/')
+        for idx, comp in enumerate(company):
+            company[idx] = comp.strip()
+    except:
+        return ['NULL']
     return company
 
 
 def size_cleaner_and_converter(row):
     """Returns the size of the game in MB(float)"""
-    size = row.text.lower()
-    size = re.sub(r'[\n]', '', size)
-    size = size.strip()
-    size_digits = float(size[:size.find(' ')])
-    size_scale = size[size.find(' ')+1:]
-    if size_scale == 'gb':
-        size_digits *= 1000
-    elif size_scale == 'kb':
-        size_digits /= 1000
+    to_mb_scale = 1000
+    try:
+        size = row.text.lower()
+        size = re.sub(r'[\n]', '', size)
+        size = size.strip()
+        size_digits = float(size[:size.find(' ')])
+        size_scale = size[size.find(' ')+1:]
+        if size_scale == 'gb':
+            size_digits *= to_mb_scale
+        elif size_scale == 'kb':
+            size_digits /= to_mb_scale
+    except:
+        return "NULL"
     return size_digits
-
-# aaa_rows = soup.find_all("div", {"class": "details__content table__row-content"})
 
 
 def game_details(soup):
-    """Calling other functions in order to parse and clean Game details data"""
-    array = dict()
+    """Calling the other functions (above)
+     in order to parse and clean the Game details section
+     Returns: game_details_section (dictionary)
+     """
+    game_details_section = dict()
     for row in soup.find_all("div", {"class": "details__content table__row-content"}):
         key = clean_key(row.previous_element)
         if key not in conf.relevant_keys:
             continue
         if key == 'genre':
-            value = game_gnere_cleaner(row)
+            value = game_genre_cleaner(row)
         elif key == 'works_on':
             value = works_on_cleaner(row)
         elif key == 'release_date':
@@ -87,23 +113,31 @@ def game_details(soup):
             value = company_cleaner(row)
         elif key == 'size':
             value = size_cleaner_and_converter(row)
-        array[key] = value
-    return array
+        game_details_section[key] = value
+    return game_details_section
 
 
 def game_sku(soup):
-    """Returns the game SKU else returns null"""
+    """Returns the game's SKU"""
     # game sku
-    game_sku = int(soup.find(class_="layout").attrs["card-product"])
-    if game_sku is None:
+    try:
+        game_sku = int(soup.find(class_="layout").attrs["card-product"])
+    except:
+    # if game_sku is None
         return 'NULL'
     return game_sku
 
 
 def game_title(soup):
-    """Returns the game title else returns null"""
-    game_title = soup.find('h1').text
-    if game_title is None:
+    """Returns the game's Title
+    Game Title is a MUST field.
+    Handling null in "game_master_scrapper"
+    function
+    """
+    try:
+        game_title = soup.find('h1').text
+    except:
+    # if game_title is None
         return 'NULL'
     return game_title
 
@@ -120,41 +154,70 @@ def game_score(soup):
 
 def game_price_base(soup):
     """Returns the game prices"""
-    game_price_base = float(soup.find("span", {"class": "product-actions-price__base-amount _price"}).text)
-    if game_price_base is None:
+    try:
+        game_price_base = float(soup.find("span", {"class": "product-actions-price__base-amount _price"}).text)
+    except:
+        # if game_price_base is None
         return "NULL"
     return game_price_base
 
 
 def game_price_final(soup):
     """Returns the game prices"""
-    game_price_final = float(soup.find("span", {"class": "product-actions-price__final-amount _price"}).text)
-    if game_price_final is None:
+    try:
+        game_price_final = float(soup.find("span", {"class": "product-actions-price__final-amount _price"}).text)
+    # if game_price_final is None:
+    except:
         return "NULL"
     return game_price_final
 
 
 def game_price_discount(game_price_base, game_price_final):
     """Returns the discount of the game. If no discount returns zero"""
-    no_discount = 0
+    no_discount = 0.00
+
     if game_price_base == game_price_final or game_price_base == 'NULL' or game_price_final == 'NULL':
         return no_discount
     else:
         return round((float(game_price_base) - float(game_price_final)) / float(game_price_base), 2)
 
+def check_game_page_url_response(game_page_url):
+    """Verify game page url response"""
+    if game_page_url.text != 200:
+        return False
+    else:
+        return True
 
-def master_page_scrapper(page):
-    soup = BeautifulSoup(page.content, features="lxml")
-    game_sql = {} # The data of the game
-    game_sql['game_sku'] = game_sku(soup)
+
+def master_page_scrapper(game_page_url):
+    """
+    Returns ALL the available data from the game's page url.
+    :param game_page_url: Response object (requests library)
+    :return: game_sql(dict) with all the game info.
+
+    """
+    game_sql = {}  # The data of the game
+
+    if not check_game_page_url_response(game_page_url):
+        raise ConnectionError(f"URL Response fail{game_page_url}")
+
+    soup = BeautifulSoup(game_page_url.content, features="lxml")
+
+    # game title
     game_sql['game_title'] = game_title(soup)
+    if game_sql['game_title'] == 'NULL':
+        raise ValueError("No game title, not printing data")
+
+    game_sql['game_sku'] = game_sku(soup)
     game_sql['game_score'] = game_score(soup)
     game_sql['game_price_base'] = game_price_base(soup)
     game_sql['game_price_final'] = game_price_final(soup)
     game_sql['game_price_discount'] = game_price_discount(game_sql['game_price_base'], game_sql['game_price_final'])
-    # Game details
+    # Game details Section
     game_details_dict = game_details(soup)
-    game_sql['game_url'] = page.url
+    game_sql['game_url'] = game_page_url.url
+
+    # Merging general data with game details section
     game_sql = {**game_sql, **game_details_dict}
     return game_sql
 
@@ -199,10 +262,14 @@ def get_pages():
 if __name__ == '__main__':
 
     for page in get_pages():
-    # for page in ["https://www.gog.com/game/new_super_luckys_tale"]:
-        rs = requests.get(page)
+    # for page in ["https://www.gog.com/game/new_super_luckys_tale"]: # todo: delete after QA
 
-        game_data = master_page_scrapper(rs)
+        try:
+            rs = requests.get(page)
+            game_data = master_page_scrapper(rs)
+
+        except Exception as ex_message:
+            print(ex_message)
 
         print(game_data)
 
